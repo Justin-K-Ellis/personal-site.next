a75fdb9c22624f9fd868c8def3be877b0df74446
Justin-K-Ellis <jlke45@gmail.com>
2026-02-04 22:35:05 +0900

    feat: create Tech interface

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,11 @@
+import { IconType } from "react-icons";
+
+export interface Tech {
+  name: string;
+  icon: IconType;
+  color: string;
+}
+
 export interface NavLink {
   url: string;
   text: string;
