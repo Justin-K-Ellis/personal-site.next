f392c2e946405e9e21992d3b7d169453738fb09b
Justin-K-Ellis <jlke45@gmail.com>
2026-02-04 22:34:19 +0900

    feat: change subtitle color

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import { useTranslations } from "next-intl";
 export default function Hero() {
   const t = useTranslations("Hero");
   return (
-    <section className="flex flex-col md:gap-4 gap-2 justify-center items-center">
+    <section className="flex flex-col md:gap-4 gap-2 justify-center items-center my-6">
       <h1 className="md:text-6xl text-4xl font-bold">{t("justin")}</h1>
       <h2 className="md:text-3xl text-xl text-blue-800 text-center w-8/10">
         {t("description")}
