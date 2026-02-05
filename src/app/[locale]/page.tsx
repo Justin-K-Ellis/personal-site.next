131ebd86fb2f589e1ffc60d50416e56c55159c80
Justin-K-Ellis <jlke45@gmail.com>
2026-02-05 22:35:13 +0900

    refactor: render lang cards w/ TechContainer

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,8 @@
 import { useTranslations } from "next-intl";
 import Hero from "@/components/Hero";
 import Title from "@/components/Title";
-import SubTitle from "@/components/SubTitle";
 import { languages } from "@/assests/techStackData";
-import TechCard from "@/components/TechCard";
+import TechContainer from "@/components/TechContainer";
 
 export default function Home() {
   const t = useTranslations("HomePage");
@@ -17,22 +16,12 @@ export default function Home() {
       <section id="tech-stack" className="flex flex-col gap-4 md:gap-8">
         <Title text={t("techStack")} />
         <p className="text-center">{t("whatIcanDo")}</p>
-        <article id="programming-languages">
-          <SubTitle text="Programming Languages" />
-          <div className="flex md:flex-row flex-col gap-2 flex-wrap justify-center">
-            {languages.map((language) => {
-              return (
-                <TechCard
-                  key={language.name}
-                  name={language.name}
-                  Icon={language.icon}
-                  color={language.color}
-                />
-              );
-            })}
-          </div>
-        </article>
-        <article id="front-end"></article>
+        <TechContainer
+          htmlId="programming-languages"
+          subtitle="Programming Languages"
+          tech={languages}
+        />
+        <hr />
       </section>
     </div>
   );
