95429a9d0f381dad30eefe0feb5496a8e0f37f9c
Justin-K-Ellis <jlke45@gmail.com>
2026-02-08 21:39:40 +0900

    feat: create TechBadge type

--- src/types.ts
+++ src/types.ts
@@ -10,3 +10,20 @@ export interface NavLink {
   url: string;
   text: string;
 }
+
+export type TechBadge =
+  | "daisyUI"
+  | "Express"
+  | "Firebase auth"
+  | "Google Maps API"
+  | "JavaScript"
+  | "Knex.js"
+  | "Next"
+  | "PostgreSQL"
+  | "React Router"
+  | "React"
+  | "shadcb/ui"
+  | "Spoontacular API"
+  | "Tailwind"
+  | "TypeScript"
+  | "Vue";
